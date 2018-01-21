90d7b46c97b930d83e6ed7287adce447a4e9d6d2
manishjha4 <manishjha.ronaldo@gmail.com>
2018-01-21 00:53:10 -0500

    made more changes to cpu player and game layout

--- pirateKing/app/src/main/java/hasan/manish/pirateking/CPU_Player.java
+++ pirateKing/app/src/main/java/hasan/manish/pirateking/CPU_Player.java
@@ -14,12 +14,11 @@ public class CPU_Player extends Player{
     public String getTheBestDecision(int numOfPlayers){
         round++;
         switch (getRank()){
-            case 0:
-                return "newRound";
             case 1:
                 if (numOfPlayers==2 && hand.get(0).getCardNum()>10)
                     return "show";
                 return "fold";
+
             case 2:
                 if (round<=2)
                     return "call";
@@ -68,15 +67,15 @@ public class CPU_Player extends Player{
                 return "call";
 
             case 7:
-                if (round<=2)
-                    return "call";
-                if (round==3)
-                    return "raise,50";
-                if (round==4)
-                    return "call";
-                if (round==5)
-                    return "raise,100";
+            if (round<=2)
                 return "call";
+            if (round==3)
+                return "raise,50";
+            if (round==4)
+                return "call";
+            if (round==5)
+                return "raise,100";
+            return "call";
 
             default: return null;
         }
