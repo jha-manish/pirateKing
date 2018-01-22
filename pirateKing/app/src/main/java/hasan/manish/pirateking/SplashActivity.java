69bc0b4fcc96eba9e760b1f1f7a07c716269b4ca
sh2rizvi@uwaterloo.ca <NeedforSped!2>
2018-01-22 01:34:03 -0500

    Updated splash screen design and activity

--- pirateKing/app/src/main/java/hasan/manish/pirateking/SplashActivity.java
+++ pirateKing/app/src/main/java/hasan/manish/pirateking/SplashActivity.java
@@ -10,17 +10,18 @@ package hasan.manish.pirateking;
         import android.view.WindowManager;
         import android.view.animation.Animation;
         import android.view.animation.AnimationUtils;
+        import android.widget.ImageView;
         import android.widget.TextView;
 
 
 public class SplashActivity extends AppCompatActivity {
-    private TextView tv;
+    private ImageView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
-        tv = (TextView) findViewById(R.id.textView);
+        tv = findViewById(R.id.imageView);
         Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
         tv.startAnimation(myanim);
         final Intent i = new Intent(SplashActivity.this,MainActivity.class);
